5cf3408b0160b71db5716ce78911a4c4a47a1ab7
Andrew <ayjtw7@gmail.com>
2022-09-24 15:01:31 -0400

    adding about information

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 
 //  homepage route
 app.get('/home', (request, response) => response.sendFile(__dirname + `/views/index.html`))
+
+
 //  about page route
 app.get('/about', (request, response) => response.sendFile(__dirname + `/views/about.html`))
 //  works page route
